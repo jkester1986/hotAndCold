f3d553c5575e244cdab15541fe5cf5f4c5648555
Julia Kester <kester@oswego.edu>
2016-11-13 15:52:13 -0500

    recognize directional keys

--- hotAndCold.js
+++ hotAndCold.js
@@ -1,1 +1,24 @@
 // JavaScript Document
+
+//event listener for ess
+document.onkeydown = function(e) {
+	e = e || window.event;
+	
+	switch (e.keyCode) {
+		case 37://left arrow
+			console.log("Left arrow was pressed");
+			break;
+		case 38://up arrow
+			console.log("Up arrow was pressed");
+			break;
+		case 39://right arrow
+			console.log("Right arrow was pressed");
+			break;
+		case 40://down arrow
+			console.log("Down arrow was pressed");
+			break;
+		default:
+			console.log("A non-directional key was pressed: " + e.keyCode);
+			break;
+	}
+}
